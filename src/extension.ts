c8f91855930bb6be65bd65fbabf5684aa4be55f4
Simon Schneider <raynigon@users.noreply.github.com>
2019-10-27 17:56:47 +0100

    Fix #5 use nginxbeautifier as formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,33 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as nginx from 'nginx-conf';
+import * as nginxbeautifier from "nginxbeautifier";
+
+function formatText(text: string) {
+    let lines = nginxbeautifier.clean_lines(text);
+    lines = nginxbeautifier.join_opening_bracket(lines);
+    lines = nginxbeautifier.perform_indentation(lines);
+    lines = nginxbeautifier.perform_alignment(lines);
+    return lines.join("\n");
+}
+
+function replaceTextInDocument(newText: string, document: vscode.TextDocument) {
+    const firstLine = document.lineAt(0);
+    const lastLine = document.lineAt(document.lineCount - 1);
+    const range = new vscode.Range(0,
+        firstLine.range.start.character,
+        document.lineCount - 1,
+        lastLine.range.end.character);
+    return vscode.TextEdit.replace(range, newText);
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
     // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('NGINX', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-            const promise = new Promise<vscode.TextEdit[]>((resolve, reject)=>{
-                nginx.parse(document.getText(), (error, tree)=>{
-                    if(error!=null){
-                        vscode.window.showErrorMessage("Unable to parse Document: "+error);
-                        reject("Unable to parse Document: "+error);
-                        return;
-                    }
-                    const text = (new nginx.NginxConfFile(tree)).toString();
-                    const firstLine = document.lineAt(0);
-                    const lastLine = document.lineAt(document.lineCount - 1);
-                    const range = new vscode.Range(0, 
-                        firstLine.range.start.character, 
-                        document.lineCount - 1, 
-                        lastLine.range.end.character);
-                    resolve([vscode.TextEdit.replace(range, text)]);
-                });
-            });
-           return promise;
+    vscode.languages.registerDocumentFormattingEditProvider('nginx', {
+        async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
+            const text = formatText(document.getText());
+            return [replaceTextInDocument(text, document)];
         }
     });
 }
